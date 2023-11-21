447e31e73a97c8fd4d159acef78913e97cc7264d
Ulises14-nov <toconasdanielulises@gmail.com>
2023-11-21 11:40:42 -0300

    ultimate

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,19 +1,27 @@
+import info from './info.json';
+
+const getInformacion = () => {
+    const datos = info;
+    return datos;
+};
+ 
+const lista = getInformacion();
+
 const AboutUs = () => {
     return (
         <section className="page-container">
             <div className="hero-container">
                 <h2 className="hero-title">Alumnos:</h2>
-                <article className="hero-section">
-                    <p className="hero-text">Troncoso Raskovsky Nahuel Santiago</p>
-                    <p>◉Me apasionan las área de desarrollo de videojuegos, de aplicaciones móviles y de programación web. 💻 📱 👾</p>
-                </article>
-                <article className="hero-section">
-                    <p className="hero-text">Toconas Ulises Facundo Daniel</p>
-                    <p>◉Egresado de la facultad de ingeniería de Palpala en el año 2022(APU), expectativas de generar experiencia en trabajos de programación, me gusta el backend, y deseo de superación constante.</p>
-                </article>
-                <article className="hero-section">
-                    <p className="hero-text">Zenteno Miguel Ángel</p>
-                    <p>◉Mis intereses son afines a la ilustración, anime, manga, creación de personajes. Con respecto al desarrollo de videojuegos, apunto más para la parte de Base de Datos, el desarrollo de personajes, y el guión.</p>
+                <article className="hero-section"> 
+                          {lista.map((obj, index) => (
+                            <div key={index}>
+                              <p>NOMBRE: {obj.nombre}</p>
+                              <p>INFORMACION: {obj.informacion}</p>
+                              <p>REPOSITORIO GITHUB:{obj.user}</p>
+                              <p>..</p>
+                            </div>
+                          ))}
+                        
                 </article>
             </div>
         </section>
